0cac25947623ba3fef019ff7a73e287199014ec9
Guti <gustavo.costanti.lara@gmail.com>
2020-12-30 10:05:00 -0300

    mongoose 01

--- Curso programador br/Módulo 26&27 - Mongo DB e Mongoose/app.js
+++ Curso programador br/Módulo 26&27 - Mongo DB e Mongoose/app.js
@@ -3,6 +3,29 @@ const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
 
+const linkSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: String,
+    url: { type: String, required: true },
+    click: { type: Number, default: 0 }
+});
+
+// Modelo
+const Link = mongoose.model('Link', linkSchema);
+
+// Documento
+const link = new Link({
+    title: "progbr",
+    description: "Link para o twitter",
+    url: "https://twitter.com/progbr"
+});
+
+link.save().then(doc => {
+    console.log(doc);
+}).catch(err => {
+    console.log(err);
+})
+
 // mongoose.connect('mongodb://127.0.0.1/links', (err, db) => {
 //     console.log(err);
 //     console.log(db);
@@ -14,16 +37,42 @@ const mongoose = require('mongoose');
 //     console.log(err);
 // })
 
-mongoose.connect('mongodb://localhost/links', { useNewUrlParser: true, useUnifiedTopology: true });
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+})
+
+const Person = mongoose.model('Person', personSchema);
+
+let person = new Person({
+    name: "José",
+    age: 23
+});
+
+person.save().then(doc => {
+    console.log(doc);
+}).catch(err => {
+    console.log(err);
+})
+
+mongoose.connect('mongodb://localhost/newlinks', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 let db = mongoose.connection;
 
 db.on("error", () => {
     console.log("houve um erro");
-})
+});
 db.once("open", () => {
     console.log("Banco carregado");
-})
+});
+
+
+
+
+
 
 
 
