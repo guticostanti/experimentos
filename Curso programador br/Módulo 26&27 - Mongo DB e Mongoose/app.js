7d10142ddf52cf2d229f9ab6024a57278afbb49a
Guti <gustavo.costanti.lara@gmail.com>
2020-12-30 09:14:08 -0300

    mongoose

--- Curso programador br/Módulo 26&27 - Mongo DB e Mongoose/app.js
+++ Curso programador br/Módulo 26&27 - Mongo DB e Mongoose/app.js
@@ -3,11 +3,27 @@ const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1/links', (err, db) => {
-    console.log(err);
-    console.log(db);
-})
+// mongoose.connect('mongodb://127.0.0.1/links', (err, db) => {
+//     console.log(err);
+//     console.log(db);
+// })
 
+// mongoose.connect('mongodb://127.0.0.1/links').then(db => {
+//     console.log(db);
+// }).catch(err => {
+//     console.log(err);
+// })
+
+mongoose.connect('mongodb://localhost/links', { useNewUrlParser: true, useUnifiedTopology: true });
+
+let db = mongoose.connection;
+
+db.on("error", () => {
+    console.log("houve um erro");
+})
+db.once("open", () => {
+    console.log("Banco carregado");
+})
 
 
 
