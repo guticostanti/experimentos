676fb898e14120df85514f206d1b3ac0d1f57e2a
Guti <gustavo.costanti.lara@gmail.com>
2021-01-03 15:07:03 -0300

    Socket 00

---
+++ Curso programador br/Módulo 30 - Socket/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const app = express();
+const socketIO = require('socket.io');
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+const server = app.listen(3000, () => {
+    console.log("running");
+})
+
+
+const io = socketIO(server)
+
+io.on('connection', (socket) => {
+
+    console.log("New connection")
+
+})
